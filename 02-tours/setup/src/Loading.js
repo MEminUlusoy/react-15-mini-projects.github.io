58ebd4c4e15d1dfd39685ceee5d0f2b46264b385
Emin Ulusoy <ulusoyeminn@gmail.com>
2023-07-16 02:00:23 +0300

    tour projesi yapıldı

---
+++ 02-tours/setup/src/Loading.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Loading = () => {
+  return (
+    <div className="loading">
+      <div class="loader">Loading
+        <span></span>
+      </div>
+    </div>
+  );
+};
+
+export default Loading;
