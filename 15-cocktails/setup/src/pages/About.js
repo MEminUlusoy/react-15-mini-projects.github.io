23b0cfe89cb27f9f8b4520d1536c04720f946b03
Emin Ulusoy <ulusoyeminn@gmail.com>
2023-08-09 02:05:40 +0300

    api projesi bitti

---
+++ 15-cocktails/setup/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet eius unde tempore, dolore facere ducimus quia esse tenetur vel quidem aperiam modi porro similique debitis, illo ea eligendi eos aliquid quod id, ab itaque ad. Porro odit harum natus fuga?
+      </p>
+    </section>
+  )
+}
+
+export default About
